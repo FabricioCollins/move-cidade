e3b9af3b398ac3539144008f85801fe89a2780f1
FabricioCollins <fabricio.collins@gmail.com>
2016-05-18 02:52:47 -0300

    Diversos ajuste no layout 2

---
+++ wp-content/themes/move-urbano/js/script.js
@@ -0,0 +1,49 @@
+var $nav,
+navH,
+$navLat,
+yinit,
+docScrollTop,
+docScrollTopLast,
+docScrollDir;
+
+$( document ).ready(function() {
+
+	// Controle as expansão do menu lateral
+	$('.nav-toggle a').click(function(event) {
+		$('.nav-main').toggleClass('open');
+	});
+
+	// Controla o componente de cadastro de email
+	$('.cadastro .toggle').click(function(event) {
+		$('.cadastro .form-wrapper').animate({
+			opacity: "toggle",
+			height: "toggle",
+			width: "toggle",
+		}, 300);
+		$(this).toggleClass('ativo').find('i').toggleClass('fa-times').toggleClass('fa-bus');
+	});
+
+	// Make search on key press after 0.5 second
+	var searchEventContainer=null;
+	$(".search-input").keyup(function(){
+		if(searchEventContainer!=null) clearTimeout(searchEventContainer);
+
+		searchEventContainer=setTimeout(function() {
+			$(".cardboard").idecCardBoard().filterByContent($(".search-input").val());
+		}, 500);
+	});
+
+
+	$nav = $(".nav-filter");
+	navH = $('.menu-main').innerHeight();
+	yinit = 0; //navH;
+
+	$(window).scroll(function() {
+		if($(window).scrollTop() > $('.header .title').innerHeight()) {
+			$nav.parent().addClass('nav-fixed');
+		} else {
+			$nav.parent().removeClass('nav-fixed');
+		}
+	});
+
+});	
